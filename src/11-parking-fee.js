6948436bcb15a575f2b9056ab055019904fe7d88
Rahul <rahuldevan2019@gmail.com>
2026-03-11 08:11:15 +0530

    passed all test cases of parking fee

--- src/11-parking-fee.js
+++ src/11-parking-fee.js
@@ -34,4 +34,37 @@
  */
 export function calculateParkingFee(hours, vehicleType) {
   // Your code here
+  if (hours <= 0) return -1;
+  if (vehicleType !== "car" && vehicleType !== "motorcycle" && vehicleType !== "bus") return -1;
+
+  const roundedHours = Math.ceil(hours);
+
+  let firstHourRate, additionalHourRate, dailyMax, fee;
+
+  if (vehicleType === "car") {
+    firstHourRate = 5;
+    additionalHourRate = 3;
+    dailyMax = 30;
+  } else if (vehicleType === "motorcycle") {
+    firstHourRate = 3;
+    additionalHourRate = 2;
+    dailyMax = 18;
+  } else if (vehicleType === "bus") {
+    firstHourRate = 10;
+    additionalHourRate = 7;
+    dailyMax = 60;
+  }
+
+  if (roundedHours === 1) {
+    fee = firstHourRate;
+  } else {
+    fee = firstHourRate + (roundedHours - 1) * additionalHourRate;
+  }
+
+  if (fee > dailyMax) {
+    fee = dailyMax;
+  }
+
+  return fee;
+
 }
